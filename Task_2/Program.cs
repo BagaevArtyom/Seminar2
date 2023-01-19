1a198c091e5ae10443ced8415e011c74e9ae96f7
Artyom <artyombagaev150@yandex.ru>
2023-01-19 15:24:34 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿       // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Цифры считать справа налево)
+        Console.Clear();
+        Console.WriteLine("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number < 100 && number > -100)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+        else
+        {
+            int result = number / 100 % 10;
+            Console.WriteLine($"Третья цифра: {result}");
+        }
