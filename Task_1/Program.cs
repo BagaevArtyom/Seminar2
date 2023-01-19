f564f16bdf17ce3daffd79b1996f645845bd4589
Artyom <artyombagaev150@yandex.ru>
2023-01-19 14:55:14 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначьное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int second = (number/10)%10;
+
+Console.WriteLine($"Вторая цифра - {second}");
